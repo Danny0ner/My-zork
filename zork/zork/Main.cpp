462b90a18b08587919bf4b1ce8260cd61489c63a
Danny0ner <dlopez3a@gmail.com>
2016-03-14 19:52:12 +0100

    Broken
    Tried to add help command to start. But now it crashes while debugging

--- zork/zork/Main.cpp
+++ zork/zork/Main.cpp
@@ -1,11 +1,15 @@
 #include"world.h"
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
 int main()
 {
 	World map;
 	map.CreateWorld();
-
+	printf("Welcome to my zork!!!\n\n");
+	printf("Move Commands: 'go north' or 'go n'  'go south' or 'go s'  'go west' or go w'  'go east' or 'go e'\nLook Commands: 'look'  'look north'  'look south'  'look west' 'look east'\nEnter 'quit' to exit.\n\n");
+	printf("%s", map.rooms[0].name);
+	
 
 	getchar();
 	return 0;
